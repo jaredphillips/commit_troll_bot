f0556687fca7a81b766cf26f00a1acae16f3310f
jaredphillips <jaredtphillips@me.com>
2013-06-10 09:15:32 -0400

    Cinch gem added

--- bot.rb
+++ bot.rb
@@ -1,18 +1,25 @@
-require 'socket'
+# after someone says hello in the channel, my github commit troll bot 
+# asks how many commits they have. it then
 
+require 'cinch'
 
-server = "chat.freenode.net"
-port = "6667"
-nick = "Ruby IRC Bot"
-channel = "#bitmaker"
+bot = Cinch::Bot.new do
+  configure do |c|
+    c.server = "irc.freenode.org"
+    c.channels = ["#cinch-bots"]
+  end
 
-s = TCPSocket.open(server, port)
-s.puts "USER HelloWillBot 0 * HellowWillBot"
-s.puts "NICK #{nick}"
-s.puts "JOIN #{channel}"
-s.puts "PRIVMSG #{channel} :Hello from Jared"
-
-until s.eof? do
-	msg = s.gets
-	puts msg
+  on :message, "hello" do |m|
+    m.reply "Hello, #{m.user.nick}"
+    puts "How many Github commits do you have?"
+    answer = gets.chomp
+    if answer.is_a?(Integer)
+    	puts "Cool, cool"
+    	puts "Last time I checked, Jared has #{answer + 1} commits on Github."
+    else
+    	puts "That's not a number"
+    end
+  end
 end
+
+bot.start
